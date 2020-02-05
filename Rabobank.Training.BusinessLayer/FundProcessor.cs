efb71af86bf9715e3f446c2c82e06dc219c1f26b
bmcarnahan <carnahan_bmc@hotmail.com>
2020-02-05 14:01:47 +0100

    Update FundProcessor.cs

--- Rabobank.Training.BusinessLayer/FundProcessor.cs
+++ Rabobank.Training.BusinessLayer/FundProcessor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,20 +19,33 @@ namespace Rabobank.Training.BusinessLayer
     public class FundProcessor : IFundsProcessor
     {
         /// <summary>
+        ///// Why the uppercase? 
         /// THIS METHOD IS USED TO UPDATE POSITION VIEWMODEL TO MAP CORRESPONDING MANDATES WITH IT PRESENT UNDER PASSED FUNDOFMANDATE.
         /// METHOD USE POSITION VIEW MODEL AND FUNDOFMANDATE OBJECTS AND RETURN UPDATED POSITIONVM BACK TO CLIENT.
         /// </summary>
+        ///// Normally if we do not document the parameters or the returns then we remove them from the comments.
         /// <param name="position"></param>
         /// <param name="fundOfmandates"></param>
         /// <returns></returns>
+        ///// You don't have to return a PositionVM because to pass a reference to the PositionVM. In fact you could make this an 
+        ///// extension on PositionVM.
         public PositionVM GetCalculatedMandates(PositionVM position, FundOfMandates fundOfmandates)
         {
+            ///// Watch out for unnecessary white space.
 
             if (position.Code == fundOfmandates.InstrumentCode && fundOfmandates.Mandates != null && fundOfmandates.Mandates.Length > 0)
             {
+                ///// You could have done this in one statement.
+                /////fundOfmandates.Mandates.ToList().ForEach(x => position.Mandates.Add(new MandateVM
+                /////{
+                /////    name = x.MandateName,
+                /////    Value = Math.Round((position.Value * x.Allocation) / 100),
+                /////    Allocation = x.Allocation / 100
+                /////}));
                 position.Mandates = new List<MandateVM>();
                 position.Mandates.AddRange
                  (
+                    ///// This variable should be camel-cased (Mandates).
                             fundOfmandates.Mandates.ToList().Select(x => new MandateVM
                             {
                                 name = x.MandateName,
@@ -49,6 +62,8 @@ namespace Rabobank.Training.BusinessLayer
                         //NOT VERY CLEAR OF THE LOGIC MENTIONED IN THE WORD DOC SHARED WITH EXERCISE. IT SAYS SUBSTRACT FROM POSITION VALUE BUT THE EXAMPLE IN THE DOC SHOWS THAT ITS STRAIGHT POSITION.VALUE * LIQUIDITY ALLOCATION.
                         //SO COMENTING THE BELOW LINE OF CODE AND KEEPING IT SIMPLE BASED ON EXAMPLE
                                 //Value = (position.Value - ((position.Value * fundOfmandates.LiquidityAllocation) / 100)),
+                        ///// What you should have done here is the following:
+                        ///// Value = position.Value - position.Mandates.Sum(x => x.Value);
                         Value = Math.Round((position.Value * fundOfmandates.LiquidityAllocation) / 100),
                         Allocation = fundOfmandates.LiquidityAllocation / 100
                     };
@@ -71,9 +86,11 @@ namespace Rabobank.Training.BusinessLayer
         /// <returns></returns>
         public PortfolioVM GetPortfolio()
         {
+            ///// Not really necessary.
             PortfolioVM portfolio = null;
             try
             {
+                ///// You could also simply return this object here.
                 portfolio = new PortfolioVM
                 {
                     Positions = new List<PositionVM> {
@@ -93,6 +110,7 @@ namespace Rabobank.Training.BusinessLayer
                 throw e;
             }
 
+            ///// There's no need to have the return outside of the try.
             return portfolio;
         }
 
